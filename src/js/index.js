9ef8dd54ccb5b334c1ce91dbc3124de325580065
NicolayHeavyFog <nikolay_on_parseltongue@mail.ru>
2022-06-04 21:27:40 +0700

    issue with pic

---
+++ src/js/index.js
@@ -0,0 +1,65 @@
+import 'normalize.css';
+import '../scss/pages/index.scss';
+
+import Swiper from 'swiper';
+import Select from './select';
+
+// eslint-disable-next-line no-unused-vars
+import fbIcon from '../assets/svg/fb.svg';
+// eslint-disable-next-line no-unused-vars
+import vkIcon from '../assets/svg/vk.svg';
+// eslint-disable-next-line no-unused-vars
+import instagramIcon from '../assets/svg/instagram.svg';
+// eslint-disable-next-line no-unused-vars
+import iconPhone from '../assets/svg/contactPhone.svg';
+// eslint-disable-next-line no-unused-vars
+import logoApp from '../assets/svg/logoApp.svg';
+// eslint-disable-next-line no-unused-vars
+import searchIcon from '../assets/svg/searchIcon.svg';
+// eslint-disable-next-line no-unused-vars
+import cartIcon from '../assets/svg/cartIcon.svg';
+// eslint-disable-next-line no-unused-vars
+import userIcon from '../assets/svg/userIcon.svg';
+
+// eslint-disable-next-line no-unused-vars
+import bc1 from '../assets/images/hero1920-1.jpg';
+
+// // eslint-disable-next-line no-unused-vars
+import bc2 from '../assets/images/hero1920-2.jpg';
+
+// // eslint-disable-next-line no-unused-vars
+import bc3 from '../assets/images/hero1920-3.jpg';
+
+// eslint-disable-next-line no-unused-vars
+const choicesTopHeader = new Select('.top-header__select', {
+  classNames: {
+    selectOuter: 'top-select',
+    selectInner: 'top-select__container',
+    dropdownContainer: 'top-select__dropdown',
+    selectedItem: 'top-select__item-selected',
+    list: 'top-select__list',
+    item: 'top-select__item',
+    dropdownOpen: 'top-select__dropdown--open',
+    dropdownClose: 'top-select__dropdown--close',
+  },
+});
+
+// eslint-disable-next-line no-unused-vars
+const choicesBottomHeader = new Select('.bottom-header__select', {
+  classNames: {
+    selectOuter: 'bottom-select',
+    selectInner: 'bottom-select__container',
+    dropdownContainer: 'bottom-select__dropdown',
+    selectedItem: 'bottom-select__item-selected',
+    list: 'bottom-select__list',
+    item: 'bottom-select__item',
+    dropdownOpen: 'bottom-select__dropdown--open',
+    dropdownClose: 'bottom-select__dropdown--close',
+  },
+  placeholderText: 'Категория',
+});
+
+// eslint-disable-next-line no-unused-vars
+const heroSlider = new Swiper('.swiper', {
+  speed: 400,
+});
---
+++ webpack.config.js
@@ -0,0 +1,176 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+// const { extendDefaultPlugins } = require('svgo');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ];
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+};
+
+function optimization() {
+  const config = {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              [
+                'svgo',
+                {
+                  plugins: [
+                    {
+                      name: 'preset-default',
+                      params: {
+                        overrides: {
+                          convertShapeToPath: {
+                            convertArcs: true,
+                          },
+                        },
+                      },
+                    },
+                    'removeViewBox',
+                    {
+                      name: 'addAttributesToSVGElement',
+                      params: {
+                        attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+  if (isProd) {
+    config.minimize = true;
+    config.minimizer.push(
+      ...[new CssMinimizerPlugin(), new TerserWebpackPlugin()]
+    );
+  }
+
+  return config;
+}
+
+const plugins = () => {
+  const basePlugins = [
+    new MiniCssExtractPlugin({
+      filename: `./${filename('css')}`,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/markup/pages/index.pug'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './assets/svg/favIcon.svg',
+      inject: true,
+    }),
+  ];
+
+  if (isDev) {
+    basePlugins.push(new ESLintPlugin());
+  }
+
+  if (isProd) {
+    // basePlugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return basePlugins;
+};
+
+module.exports = () => ({
+  context: path.resolve(__dirname, 'src'),
+  entry: ['@babel/polyfill', './js/index.js'],
+  output: {
+    filename: `${filename('js')}`,
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: `assets/${filename('[ext]')}`,
+    publicPath: '',
+    clean: true,
+  },
+  resolve: {
+    // extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: jsLoaders(),
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/,
+        use: ['svg-sprite-loader', 'svgo-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.pug$/i,
+        loader: 'pug-loader',
+      },
+    ],
+  },
+  optimization: optimization(),
+  plugins: plugins(),
+  devtool: isProd ? false : 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8000,
+  },
+});
