6a30bf38d4f74745c9b285aaa44f44c0c0478368
NicolayHeavyFog <nikolay_on_parseltongue@mail.ru>
2022-06-05 14:06:17 +0700

    Seat Down Please

---
+++ sdp/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  // parser: "@babel/eslint-parser",
+  // parserOptions: {
+  //   ecmaVersion: "latest",
+  //   sourceType: "module",
+  //   allowImportExportEverywhere: false,
+  //   ecmaFeatures: {
+  //     globalReturn: false,
+  //     requireConfigFile: false,
+  //   },
+  //   babelOptions: {
+
+  //   }
+  // },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'no-alert': 0,
+    'no-param-reassign': [2, { props: false }],
+    'no-plusplus': 0,
+    'no-iterator': 0,
+    'no-restricted-syntax': [2, 'WithStatement'],
+    'func-style': 0,
+    'func-names': ['error', 'never'],
+    'no-var': 'error',
+    'prettier/prettier': 'error',
+  },
+};
---
+++ sdp/.stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["stylelint-config-standard-scss",
+            "stylelint-config-prettier-scss",
+            "stylelint-config-rational-order",
+            "stylelint-prettier/recommended"],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+    // Дополнительные правила
+  }
+};
